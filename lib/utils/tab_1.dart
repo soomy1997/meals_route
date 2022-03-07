bc5ced6f51060ff7999f5a59a140fab0bb4a77c3
Asma Alkhaldi <soomy4444@gmail.com>
2022-03-07 22:59:46 +0300

    ios worked

--- lib/utils/tab_1.dart
+++ lib/utils/tab_1.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:meals_route/models/all_meals.dart';
 
@@ -24,201 +22,100 @@ class Tab1 extends StatelessWidget {
     ];
     TextTheme textheme = Theme.of(context).textTheme;
 
-    return Padding(
+    
+    return SingleChildScrollView(
+      child: Padding(
         padding: const EdgeInsets.only(left: 20.0, right: 20),
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '435',
-                  textAlign: TextAlign.center,
-                  style: textheme.headline2?.copyWith(color: red),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Text(
-                  'Total Meals',
-                  textAlign: TextAlign.center,
-                  style: textheme.headline2,
-                ),
-              ],
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '435',
+                    textAlign: TextAlign.center,
+                    style: textheme.headline2?.copyWith(color: red),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'Total Meals',
+                    textAlign: TextAlign.center,
+                    style: textheme.headline2,
+                  ),
+                ],
+              ),
             ),
-          ),
-          SingleChildScrollView(
-            child: StreamBuilder(
-                stream:
-                    FirebaseFirestore.instance.collection('Users').snapshots(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Text('Loading....');
-                  }
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  }
-                  final docses = snapshot.data.docs;
-                  print(docses);
-                  return ListView.builder(
-                    itemBuilder: (context, index) {
-                      return SizedBox(
-                        child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              //mainAxisAlignment: MainAxisAlignment.center,
+            SingleChildScrollView(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          //mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
                               children: [
-                                Column(
-                                  children: [
-                                    SizedBox(
-                                      height: 100,
-                                      width: 90,
-                                      child: CircleAvatar(
-                                        backgroundColor: Colors.transparent,
-                                        child: SizedBox(
-                                            width: 70,
-                                            height: 70,
-                                            child: ClipOval(
-                                              child: Image.asset(
-                                                allMeals[index].imgUrl,
-                                              ),
-                                            )),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                    left: 15.0,
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        docses[index]["Name"],
-                                        style: textheme.subtitle1,
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(top: 17),
-                                        child: Text(
-                                          allMeals[index].mealNumber,
-                                          style: textheme.subtitle2
-                                              ?.copyWith(color: red),
-                                        ),
-                                      )
-                                    ],
+                                SizedBox(
+                                  height: 100,
+                                  width: 90,
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.transparent,
+                                    child: SizedBox(
+                                        width: 70,
+                                        height: 70,
+                                        child: ClipOval(
+                                          child: Image.asset(
+                                            allMeals[index].imgUrl,
+                                          ),
+                                        )),
                                   ),
                                 )
                               ],
-                            )),
-                      );
-                    },
-                    itemCount: docses.length,
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.all(5),
-                    scrollDirection: Axis.vertical,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 15.0,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    allMeals[index].mealType,
+                                    style: textheme.subtitle1,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 17),
+                                    child: Text(
+                                      allMeals[index].mealNumber,
+                                      style: textheme.subtitle2
+                                          ?.copyWith(color: red),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        )),
                   );
-                }),
-          ),
-        ]));
-    // return SingleChildScrollView(
-    //   child: Padding(
-    //     padding: const EdgeInsets.only(left: 20.0, right: 20),
-    //     child: Column(
-    //       children: [
-    //         Padding(
-    //           padding: const EdgeInsets.all(15.0),
-    //           child: Row(
-    //             mainAxisAlignment: MainAxisAlignment.center,
-    //             children: [
-    //               Text(
-    //                 '435',
-    //                 textAlign: TextAlign.center,
-    //                 style: textheme.headline2?.copyWith(color: red),
-    //               ),
-    //               const SizedBox(
-    //                 width: 20,
-    //               ),
-    //               Text(
-    //                 'Total Meals',
-    //                 textAlign: TextAlign.center,
-    //                 style: textheme.headline2,
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //         SingleChildScrollView(
-    //           child: ListView.builder(
-    //             itemBuilder: (context, index) {
-    //               return SizedBox(
-    //                 child: Card(
-    //                     shape: RoundedRectangleBorder(
-    //                       borderRadius: BorderRadius.circular(20),
-    //                     ),
-    //                     child: Row(
-    //                       crossAxisAlignment: CrossAxisAlignment.center,
-    //                       //mainAxisAlignment: MainAxisAlignment.center,
-    //                       children: [
-    //                         Column(
-    //                           children: [
-    //                             SizedBox(
-    //                               height: 100,
-    //                               width: 90,
-    //                               child: CircleAvatar(
-    //                                 backgroundColor: Colors.transparent,
-    //                                 child: SizedBox(
-    //                                     width: 70,
-    //                                     height: 70,
-    //                                     child: ClipOval(
-    //                                       child: Image.asset(
-    //                                         allMeals[index].imgUrl,
-    //                                       ),
-    //                                     )),
-    //                               ),
-    //                             )
-    //                           ],
-    //                         ),
-    //                         Padding(
-    //                           padding: const EdgeInsets.only(
-    //                             left: 15.0,
-    //                           ),
-    //                           child: Column(
-    //                             crossAxisAlignment: CrossAxisAlignment.start,
-    //                             children: [
-    //                               Text(
-    //                                 allMeals[index].mealType,
-    //                                 style: textheme.subtitle1,
-    //                               ),
-    //                               Padding(
-    //                                 padding: const EdgeInsets.only(top: 17),
-    //                                 child: Text(
-    //                                   allMeals[index].mealNumber,
-    //                                   style: textheme.subtitle2
-    //                                       ?.copyWith(color: red),
-    //                                 ),
-    //                               )
-    //                             ],
-    //                           ),
-    //                         )
-    //                       ],
-    //                     )),
-    //               );
-    //             },
-    //             itemCount: allMeals.length,
-    //             shrinkWrap: true,
-    //             padding: const EdgeInsets.all(5),
-    //             scrollDirection: Axis.vertical,
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
+                },
+                itemCount: allMeals.length,
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(5),
+                scrollDirection: Axis.vertical,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
